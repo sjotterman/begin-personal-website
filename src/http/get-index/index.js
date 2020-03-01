7323018b7066c2409286e0aeb301abe704b19884
Samuel Otterman <sjotterman@users.noreply.github.com>
2020-03-01 16:02:44 -0500

    initial edits

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -7,10 +7,10 @@ exports.handler = async function Index () {
     /**
      * Basic bio
      */
-    fullname: 'Your Name', // ←  Start by adding your name!
+    fullname: 'Samuel Otterman', // ←  Start by adding your name!
     title: 'My personal site!',
-    occupation: 'Artist & Photographer',
-    location: 'West Glacier, MT',
+    occupation: 'Full Stack Developer',
+    location: 'Medina, Ohio',
     bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.',
 
     /**
